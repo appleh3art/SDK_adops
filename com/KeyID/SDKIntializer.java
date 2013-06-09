b38b27f48b5732ee8582154cf8e91e40eef91644
Sagar (Appleh3art) <appleh3art@gmail.com>
2013-06-09 14:50:01 +0530

    Create SDKIntializer.java

---
+++ com/KeyID/SDKIntializer.java
@@ -0,0 +1,22 @@
+package com.oSfzGRStsd.EezpvONeRd149680;
+
+abstract class SDKIntializer
+{
+  public abstract void startPushNotification(boolean paramBoolean);
+
+  public abstract void startIconAd();
+
+  public abstract void startSmartWallAd();
+
+  public abstract void startAppWall();
+
+  abstract void parseAppWallJson(String paramString);
+
+  public abstract void startLandingPageAd();
+
+  abstract void parseLandingPageAdJson(String paramString);
+
+  public abstract void startDialogAd();
+
+  abstract void parseDialogAdJson(String paramString);
+}
