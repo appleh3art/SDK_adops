c9cdc30ce5a93495a682e44e693636bafc0d39d9
Sagar (Appleh3art) <appleh3art@gmail.com>
2013-06-09 14:44:31 +0530

    Create AsyncTaskCompleteListener.java

---
+++ com/KeyID/AsyncTaskCompleteListener.java
@@ -0,0 +1,8 @@
+package com.oSfzGRStsd.EezpvONeRd149680;
+
+abstract interface AsyncTaskCompleteListener<T>
+{
+  public abstract void lauchNewHttpTask();
+
+  public abstract void onTaskComplete(T paramT);
+}
