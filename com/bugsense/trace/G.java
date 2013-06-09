0299f6c83de2b0bd7b1ea3305117c673d681e7af
Sagar (Appleh3art) <appleh3art@gmail.com>
2013-06-09 14:43:07 +0530

    Create G.java

---
+++ com/bugsense/trace/G.java
@@ -0,0 +1,17 @@
+package com.bugsense.trace;
+
+public class G
+{
+  public static String FILES_PATH = "null";
+  public static String APP_VERSION = "unknown";
+  public static String APP_PACKAGE = "unknown";
+  public static String URL = "https://bugsense.appspot.com/api/errors/airpush";
+  public static String TAG = "BugSenseHandler";
+  public static String ANDROID_VERSION = null;
+  public static String PHONE_MODEL = null;
+  public static String PHONE_BRAND = null;
+  public static String API_KEY = null;
+  public static String TraceVersion = "1.1.1";
+  public static String APPID = "";
+  public static boolean HAS_ROOT = false;
+}
